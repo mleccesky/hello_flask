1362781e3aa843dbeef5cfec3b4f4996a885f5dc
Marco Lecce <marco.lecce@capgemini.com>
2025-04-28 15:10:25 +0200

    URL Building Flask tutorial step

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,17 @@
+# My first Flask web server
 from flask import Flask
 from markupsafe import escape
+from flask import url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Index Page"
+
+@app.route('/login')
+def login():
+    return 'login'
 
 @app.route("/hello")
 def hello():
@@ -16,9 +22,13 @@ def hello_name(name):
     return f"Hello, {escape(name)}!"
 
 @app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'User {escape(username)}'
+def profile(username):
+    return f'{username}\'s profile'
+# def show_user_profile(username):
+#     # show the user profile for that user
+#     return f'User {escape(username)}'
+
+
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
@@ -29,3 +39,18 @@ def show_post(post_id):
 def show_subpath(subpath):
     # show the subpath after /path/
     return f'Subpath {escape(subpath)}'
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+# URL redirects
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
