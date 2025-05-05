f168cf2c171a83663482abb206d3af4ddcd8d5cf
Marco Lecce <marco.lecce@skytv.it>
2025-05-05 12:28:10 +0200

    tested also multiple http verb function for same url route

--- wsgi.py
+++ wsgi.py
@@ -10,20 +10,20 @@ app = Flask(__name__)
 def index():
     return "Index Page"
 
-# @app.route("/login", methods=['GET','POST'])
-# def login():
-#     if request.method == 'POST':
-#         return do_the_login()
-#     else:
-#         return show_the_login_form()
+@app.route("/login", methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
 
-@app.get('/login')
-def login_get():
-    return show_the_login_form()
+# @app.get('/login')
+# def login_get():
+#     return show_the_login_form()
 
-@app.post('/login')
-def login_post():
-    return do_the_login()
+# @app.post('/login')
+# def login_post():
+#     return do_the_login()
 
 
 def do_the_login():
