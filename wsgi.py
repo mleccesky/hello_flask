b47ee85762cd08b3567df7a4eafcbd606d1b943a
Marco Lecce <marco.lecce@skytv.it>
2025-05-05 18:09:36 +0200

    request method and form usage

--- wsgi.py
+++ wsgi.py
@@ -9,19 +9,47 @@ app = Flask(__name__)
 def index():
     return "Index Page"
 
-@app.route("/login", methods=['GET','POST'])
-def login():
+@app.route("/login_no_cred", methods=['GET','POST'])
+def login_no_cred():
     if request.method == 'POST':
         return do_the_login()
     else:
         return show_the_login_form()
+
 
 def do_the_login():
     return "login procedure start..."
 
 def show_the_login_form():
     return "login form:"
-#prova
+
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+        request.form['password']):
+            return render_template('login.html', username = request.form['username'])
+        else:
+            error = "Invalid username/password"
+
+    return render_template('login.html', username = "BAD BOY NOT LOGGED or GET REQUEST. I KNOW YOU!")
+
+def log_the_user_in(username=None):
+    if not username:
+        raise ValueError("No username field in http request data form!")
+    else:
+        return      
+
+def valid_login(username,password):
+    if username == "user" and password == "pwd":
+        return True
+    else:
+        return False
+
+
 
 @app.route('/hello/')
 @app.route("/hello/<name>")
