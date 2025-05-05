46941d88b6ad2c3a058db80eb062cd3c98cadc65
Marco Lecce <marco.lecce@skytv.it>
2025-05-05 12:25:45 +0200

    tested http verbs in flask

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from markupsafe import escape
 from flask import url_for
+from flask import request
 
 app = Flask(__name__)
 
@@ -9,48 +10,24 @@ app = Flask(__name__)
 def index():
     return "Index Page"
 
-@app.route('/login')
-def login():
-    return 'login'
+# @app.route("/login", methods=['GET','POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     else:
+#         return show_the_login_form()
 
-@app.route("/hello")
-def hello():
-    return "Hello, World"
+@app.get('/login')
+def login_get():
+    return show_the_login_form()
 
-@app.route("/<name>")
-def hello_name(name):
-    return f"Hello, {escape(name)}!"
-
-@app.route('/user/<username>')
-def profile(username):
-    return f'{username}\'s profile'
-# def show_user_profile(username):
-#     # show the user profile for that user
-#     return f'User {escape(username)}'
+@app.post('/login')
+def login_post():
+    return do_the_login()
 
 
+def do_the_login():
+    return "login procedure start..."
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
-
-@app.route('/projects/')
-def projects():
-    return 'The project page'
-
-@app.route('/about')
-def about():
-    return 'The about page'
-
-# URL redirects
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='John Doe'))
+def show_the_login_form():
+    return "login form:"
